6c99b3a40d2e292cf325977497d46c2187370773
tatsuro1997 <minagawa@dptr.jp>
2022-03-04 09:02:25 +0900

    Add onClick

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,10 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpsenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('Clicked!!!');
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
@@ -12,6 +16,7 @@ const ExpsenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>$ {props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
