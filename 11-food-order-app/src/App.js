342d936c49d3471521fba52b1f67969eab3b9954
tatsuro1997 <minagawa@dptr.jp>
2022-03-09 12:06:26 +0900

    Managing Car & Modal State

--- 11-food-order-app/src/App.js
+++ 11-food-order-app/src/App.js
@@ -1,16 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarhandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCarhandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCarhandler} />}
+      <Header onShowCart={showCarhandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </Fragment>
   );
