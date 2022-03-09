6404fccad25e15b2ab335eb6026592d83f2ff32a
tatsuro1997 <minagawa@dptr.jp>
2022-03-10 07:19:48 +0900

    Component Updates in Action

--- 12/src/App.js
+++ 12/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button'
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const togglrParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={togglrParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
