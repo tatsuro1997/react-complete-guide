a044316a6f14dd715e5c35cda161d4cf28800a42
tatsuro1997 <minagawa@dptr.jp>
2022-03-11 11:19:09 +0900

    Realistic Exsample

--- 14/src/App.js
+++ 14/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-e3621-default-rtdb.firebaseio.com/movies.json');
+      const response = await fetch('https://react-http-e3621-default-rtdb.firebaseio.coms/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -43,7 +43,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-e3621-default-rtdb.firebaseio.com/movies.json', {
+    const response = await fetch('https://react-http-e3621-default-rtdb.firebaseio.coms/movies.json', {
       method: 'POST',
       body: JSON.stringify(movie),
       headres: {
