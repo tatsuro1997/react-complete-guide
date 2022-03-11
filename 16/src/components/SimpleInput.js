62ee205d5514735a3bc489d945028f42fe4a1208
tatsuro1997 <minagawa@dptr.jp>
2022-03-11 15:43:00 +0900

    Managing The Overall From Validity

--- 16/src/components/SimpleInput.js
+++ 16/src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvaild = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+    if (enteredNameIsValid) {
+      formIsValid = true
+    }
+
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   }
@@ -50,7 +56,7 @@ const SimpleInput = (props) => {
         {nameInputIsInvaild && <p className='error-text'>Name must not to be empty.</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
