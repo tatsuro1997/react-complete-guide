cfc206fd21730840022a2e22fdaabd4fb41c726c
tatsuro1997 <minagawa@dptr.jp>
2022-03-15 09:37:23 +0900

    Using useEffect with Redux

--- 19/src/components/Shop/ProductItem.js
+++ 19/src/components/Shop/ProductItem.js
@@ -1,54 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { cartActions  } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-
-    const newTotalQuantity = cart.totalQuantity + 1;
-
-    const updatedItems = cart.items.slice();
-    const existingItem = updatedItems.find((item) => item.id === id);
-    if (existingItem) {
-      const updatedItem = { ...existingItem };
-      updatedItem.quantity++;
-      updatedItem.price = updatedItem.price + price;
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        name: title
-      });
-    }
-
-    const newCart = {
-      totalQuantity: newTotalQuantity,
-      items: updatedItems,
-    };
-
-    dispatch(cartActions.replaceCart(newCart));
-
-
-    // dispatch(
-    //   cartActions.addItemToCart({
-    //     id,
-    //     title,
-    //     price,
-    //   })
-    // );
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
