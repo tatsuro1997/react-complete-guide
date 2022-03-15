559f53661860b49ab78d351167f5b5bf33b524b0
tatsuro1997 <minagawa@dptr.jp>
2022-03-15 16:25:22 +0900

    Using an Action Creator Thunk

--- 19/src/store/cart-slice.js
+++ 19/src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui-slice";
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
@@ -41,6 +43,49 @@ const cartSlice = createSlice({
         }
     }
 });
+
+export const sendCartDate = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending cart data!'
+            })
+        );
+
+        const sendRequest = async () => {
+            const response = await fetch('https://react-http-e3621-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending cart data faild.');
+            }
+        };
+
+        try {
+            await sendRequest();
+
+            dispatch(
+                uiActions.showNotification({
+                    status: 'success',
+                    title: 'Success!',
+                    message: 'Sent cart data successfully!'
+                })
+            );
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart data failed!'
+                })
+            );
+        }
+    };
+};
 
 export const cartActions = cartSlice.actions;
 
