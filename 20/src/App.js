68d7134f6022736f17ba23acc0df67283adaf9e5
tatsuro1997 <minagawa@dptr.jp>
2022-03-17 09:18:38 +0900

    Updating To React Router v6

--- 20/src/App.js
+++ 20/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import MainHeader from './components/MainHeader';
 import ProductDetail from './pages/ProductDetail';
@@ -10,20 +10,14 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
+        <Routes>
+          <Route path='/' element={<Navigate to='/welcome' />} />
+          <Route path="/welcome/*" element={<Welcome />} >
+            <Route path="new-user" element={<p>Welcome, new user!</p>} />
           </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+          <Route path="/products" element={<Products />} />
+          <Route path='/products/:productId' element={<ProductDetail />} />
+        </Routes>
       </main>
 
     </div>
@@ -31,4 +25,3 @@ function App() {
 }
 
 export default App;
-
--- 20/src/pages/Products.js
+++ 20/src/pages/Products.js
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
+    const navigate = useNavigate();
+    navigate('/welcome', {replace: true});
     return (
         <section>
             <h1>The Products Page</h1>
