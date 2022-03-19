098d2ae0182b26e6bfac0e08c7630162d9623455
tatsuro1997 <minagawa@dptr.jp>
2022-03-20 07:41:27 +0900

    Extra Dynamic Parameter Value

--- 23/pages/news/[newsId].js
+++ 23/pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 function DetailsPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
     return <h1>The Details Page</h1>
 }
 
