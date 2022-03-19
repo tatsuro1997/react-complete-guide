01e997d0a96ae96eb63a717f1721e2ab797a6bcb
tatsuro1997 <minagawa@dptr.jp>
2022-03-20 07:51:43 +0900

    Linking Between Pages

--- 23/pages/news/index.js
+++ 23/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import { Fragment } from "react";
+
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-freamework">
+                        NextJS is a great Framework
+                    </Link>
+                </li>
+                <li>Something else</li>
+            </ul>
+        </Fragment>
+    );
 }
 
 export default NewsPage;
