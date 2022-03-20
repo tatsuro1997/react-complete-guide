2a72328ac556aad9073b53704ff1c3249f981048
tatsuro1997 <minagawa@dptr.jp>
2022-03-21 07:06:26 +0900

    Outputting a list of meetups

---
+++ 23-1/pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/2560px-Munchen_collage.jpg',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/2560px-Munchen_collage.jpg',
+        address: 'Some address 1, 54321 Some City',
+        description: 'This is a Second meetup'
+    },
+];
+
+function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage;
