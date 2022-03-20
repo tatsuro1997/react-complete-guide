56562011bbfd4f3924c67e841ee8f17ed35fa074
tatsuro1997 <minagawa@dptr.jp>
2022-03-21 07:49:14 +0900

    Adding Custom Components & CSS Modules

---
+++ 23-1/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/2560px-Munchen_collage.jpg'
+            title='A First Meetup'
+            address='Some Street 5, Some City'
+            description='The meetup description'
+         />
+    );
+};
+
+export default MeetupDetails;
