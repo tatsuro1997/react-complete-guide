de11c81c97f03251b21bad782f40f8908fd4af75
tatsuro1997 <minagawa@dptr.jp>
2022-03-22 06:25:24 +0900

    Working with Params for SSG Data Fetching

--- 23-1/pages/[meetupId]/index.js
+++ 23-1/pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
     );
 };
 
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/2560px-Munchen_collage.jpg',
+                id: meetupId,
+                title: 'A First Meetup',
+                address: 'Some Street 5, Some City',
+                description: 'The meetup description',
+            }
+        }
+    }
+
+}
+
 export default MeetupDetails;
