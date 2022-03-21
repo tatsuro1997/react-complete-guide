d22e38d91ef9ff8b8640c228bf1f0fb7a0f0ac51
tatsuro1997 <minagawa@dptr.jp>
2022-03-22 07:19:29 +0900

    Working with MongoDB

--- 23-1/pages/api/new-meetup.js
+++ 23-1/pages/api/new-meetup.js
@@ -1,12 +1,23 @@
+import { MongoClient } from 'mongodb'
+
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
 
-        const { title, image, address, description } = data;
+        const client = await MongoClient.connect(`mongodb+srv://${process.env.NEXT_PUBLIC_MONGODB_USER_NAME}:${process.env.NEXT_PUBLIC_MONGODB_USER_PASSWORD}@cluster0.bnhpu.mongodb.net/meetups?retryWrites=true&w=majority`);
+        const db = client.db();
 
-        
+        const meetupsCollection = db.collection('meetups');
+
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({ message: 'Meetup inserted!' });
     }
 }
 
