36c8f4ad3a434f153fb898ef4756710bdcbd3356
tatsuro1997 <minagawa@dptr.jp>
2022-03-22 05:56:24 +0900

    How Pre-rendering Works & Which Problem We Face

--- 23-1/pages/index.js
+++ 23-1/pages/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -18,7 +20,14 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />;
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        // sed a http request
+        setLoadedMeetups(DUMMY_MEETUPS);
+    }, [])
+
+    return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
