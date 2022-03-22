68797d3ca44d5458cc53e17089f7d48a244e09b4
tatsuro1997 <minagawa@dptr.jp>
2022-03-23 06:43:32 +0900

    Adding Head Tags

--- 23-1/pages/new-meetup/index.js
+++ 23-1/pages/new-meetup/index.js
@@ -1,4 +1,7 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from 'next/head';
+
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
@@ -20,7 +23,18 @@ function NewMeetupPage() {
         router.push('/');
     }
 
-    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    return (
+        <Fragment>
+            <Head>
+                <title>Add a New Meetup</title>
+                <meta
+                    name="description"
+                    content="Add your own meetups and create amazing networking oppotunities."
+                />
+            </Head>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </Fragment>
+    )
 }
 
 export default NewMeetupPage;
