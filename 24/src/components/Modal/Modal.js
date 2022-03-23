41bd4e0528e64bbd199fd879e7bc7e1e06c78a18
tatsuro1997 <minagawa@dptr.jp>
2022-03-23 09:29:04 +0900

    Animation Timing

--- 24/src/components/Modal/Modal.js
+++ 24/src/components/Modal/Modal.js
@@ -3,6 +3,11 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+};
+
 const modal = (props) => {
 
     return (
@@ -10,7 +15,7 @@ const modal = (props) => {
             mountOnEnter
             unmountOnExit
             in={props.show}
-            timeout={300}
+            timeout={animationTiming}
             >
             {state => {
                 const cssClasses = [
