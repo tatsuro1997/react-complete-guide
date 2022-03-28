044cfb77e03538d5be5619eb3cbd4f26034bb7e0
tatsuro1997 <minagawa@dptr.jp>
2022-03-29 08:57:18 +0900

    Preparing the APP for HTTP

--- 29-1/src/pages/NewMeetup.js
+++ 29-1/src/pages/NewMeetup.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupFrom";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     )
 }
