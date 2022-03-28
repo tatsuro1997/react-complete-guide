8995732f8dc8a60ed112ce148d18e3a9fc7eb0d8
tatsuro1997 <minagawa@dptr.jp>
2022-03-29 06:46:21 +0900

    Building Wrapper Components with props.children & Adding a Form

--- 29-1/src/App.js
+++ 29-1/src/App.js
@@ -1,25 +1,26 @@
 import { Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
-import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
