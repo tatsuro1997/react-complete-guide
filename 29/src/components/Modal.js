494fad3427482998c0ada63e5d2a6b1f045b366f
tatsuro1997 <minagawa@dptr.jp>
2022-03-28 09:06:44 +0900

    Working with "Event Props" (Passing Function As Props)

--- 29/src/components/Modal.js
+++ 29/src/components/Modal.js
@@ -1,9 +1,17 @@
 function Modal(props) {
+    function cancelHandler() {
+        props.onCancel();
+    }
+
+    function confirmHandler() {
+        props.onConfirm();
+    }
+
     return (
         <div className="modal">
             <p>Are you sure?</p>
-            <button className="btn btn--alt">Cancel</button>
-            <button className="btn">Comfirm</button>
+            <button className="btn btn--alt" onClick={cancelHandler}>Cancel</button>
+            <button className="btn" onClick={confirmHandler}>Comfirm</button>
         </div>
     )
 };
