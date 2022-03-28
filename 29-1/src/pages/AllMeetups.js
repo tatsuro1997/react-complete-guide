d86e73e39dbe134e3782f07b463eff903bd157a7
tatsuro1997 <minagawa@dptr.jp>
2022-03-29 05:56:38 +0900

    Outputting Lists of Data

--- 29-1/src/pages/AllMeetups.js
+++ 29-1/src/pages/AllMeetups.js
@@ -1,5 +1,31 @@
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'Test',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Coat_types_3.jpg/1920px-Coat_types_3.jpg',
+        address: 'Test 4, 1234 City',
+        description: 'Test Test Test'
+    },
+    {
+        id: 'm2',
+        title: 'Test2',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Coat_types_3.jpg/1920px-Coat_types_3.jpg',
+        address: 'Test 22, 1234 City',
+        description: 'Test2 Test2 Test2'
+    }
+]
+
 function AllMeetupsPage() {
-    return <div>All Meetups Page</div>
+    return (
+        <section>
+            <h1>All Meetups</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => {
+                    return <li key={meetup.id}>{meetup.title}</li>;
+                })}
+            </ul>
+        </section>
+    );
 }
 
 export default AllMeetupsPage;
