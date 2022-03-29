247e395b15ce4246d0223b77455575cbbb47a848
tatsuro1997 <minagawa@dptr.jp>
2022-03-29 09:16:07 +0900

    Sending a POST Request

--- 29-1/src/pages/NewMeetup.js
+++ 29-1/src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupFrom";
 
 function NewMeetupPage() {
     function addMeetupHandler(meetupData) {
-        
+        fetch(
+            'https://react-http-e3621-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
 
     return (
